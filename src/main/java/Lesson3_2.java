e76474e65c1c6d560c2f9a0066df5a30e309bb5b
Юрий <sheviurii@gmail.com>
2019-06-12 16:41:01 +0300

    Lesson 3_2 doesnt finished

---
+++ src/main/java/Lesson3_2.java
@@ -0,0 +1,38 @@
+//public class Lesson3_2 {
+//
+//    public class Human {
+//        private int weight;
+//        private int age;
+//        private String first_name;
+//        private String last_name;
+//        private int height;
+//
+//            public Human(int height, int weight, int age){
+//
+//            }
+//
+//        public class Boxer {
+//            public int age;
+//            public int weight;
+//            public int strength;
+//
+//            public Boxer(int age, int weight, int strength) {
+//                this.age = age;
+//                this.weight = weight;
+//                this.strength = strength;
+//            }
+//
+//            public boolean fight(Boxer anotherBoxer) {
+//                // write your code here
+//            }
+//
+//            public static void main(String[] args) {
+//                fight();
+//            }
+//        }
+//    }
+//}
+//
+//
+//
+//
